3cd99f56ae1dde04f94f50c1803a783bb8bc85c9
Danny Hermes <daniel.j.hermes@gmail.com>
2014-03-26 11:51:29 -0700

    Got 3D scatter working and started adding stuff in write-up.

--- simple_3danim.py
+++ simple_3danim.py
@@ -15,7 +15,7 @@ def Gen_RandLine(length, dims=2) :
     """
     lineData = np.empty((dims, length))
     lineData[:, 0] = np.random.rand(dims)
-    for index in range(1, length) :
+    for index in range(1, length):
         # scaling the random numbers by 0.1 so
         # movement is small compared to position.
         # subtraction by 0.5 is to change the range to [-0.5, 0.5]
@@ -25,11 +25,13 @@ def Gen_RandLine(length, dims=2) :
 
     return lineData
 
-def update_lines(num, dataLines, lines) :
-    for line, data in zip(lines, dataLines) :
+def update_lines(num, dataLines, lines):
+    for line, data in zip(lines, dataLines):
         # NOTE: there is no .set_data() for 3 dim data...
-        line.set_data(data[0:2, :num])
-        line.set_3d_properties(data[2,:num])
+        # line.set_data(data[0:2, :num])
+        line.set_data(data[0:2, num:(num + 1)])
+        # line.set_3d_properties(data[2,:num])
+        line.set_3d_properties(data[2,num:(num + 1)])
     return lines
 
 # Attaching 3D axis to the figure
@@ -41,7 +43,8 @@ data = [Gen_RandLine(25, 3) for index in range(50)]
 
 # Creating fifty line objects.
 # NOTE: Can't pass empty arrays into 3d version of plot()
-lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
+lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1], 'o')[0]
+         for dat in data]
 
 # Setting the axes properties
 ax.set_xlim3d([0.0, 1.0])
@@ -56,7 +59,8 @@ ax.set_zlabel('Z')
 ax.set_title('3D Test')
 
 # Creating the Animation object
-line_ani = animation.FuncAnimation(fig, update_lines, 25, fargs=(data, lines),
-                              interval=50, blit=False)
+line_ani = animation.FuncAnimation(fig, update_lines, 25,
+                                   fargs=(data, lines),
+                                   interval=50, blit=False)
 
 plt.show()
