bbc39ba0a761a47321c2a36e35500f22432e36d9
Danny Hermes <daniel.j.hermes@gmail.com>
2014-04-09 14:54:37 -0700

    Full dendrite working with merged cones geometry.

--- full_dendrite_mesh.py
+++ full_dendrite_mesh.py
@@ -10,33 +10,41 @@ else:
 
 
 SCALE_FACTOR = 50  # dolfin.Mesh has issues below a certain scale
-SPHERE_Z = SCALE_FACTOR * 1 - np.sqrt(0.05 * SCALE_FACTOR**2)
-SPHERE_RADIUS = SCALE_FACTOR * 0.3
-# Make artificially lower, since it has to intersect the shaft
+# Top cone data for synapse
+TOP_CONE_TOP_Z = SCALE_FACTOR * 1.0
+TOP_CONE_TOP_RADIUS = SCALE_FACTOR * 0.2
+# Cone boundary data for synapse
+CONE_BOUNDARY_Z = SCALE_FACTOR * (1.0 - 0.16)
+CONE_BOUNDARY_RADIUS = SCALE_FACTOR * 0.3
+# Bottom cone data for synapse
+BOTTOM_CONE_BOTTOM_Z = SCALE_FACTOR * (1.0 - 3 * 0.16)
+BOTTOM_CONE_BOTTOM_RADIUS = SCALE_FACTOR * 0.1
+# Cylinder data for dendritic shaft
+CYLINDER_TOP = BOTTOM_CONE_BOTTOM_Z
+# Make bottom artificially lower, since it has to intersect the shaft
 # beneath it.
-CYLINDER_BOTTOM = SCALE_FACTOR * (-0.2)
-CYLINDER_TOP = SPHERE_Z - np.sqrt(0.08 * SCALE_FACTOR**2)
+CYLINDER_BOTTOM = SCALE_FACTOR * (-0.1)
 CYLINDER_RADIUS = SCALE_FACTOR * 0.1
-BOX_WIDTH = SCALE_FACTOR * 2.0
-BOX_BOTTOM = SCALE_FACTOR * 1.0
+# Cylinder data for dendrite (main line)
 DENDRITE_RADIUS = SCALE_FACTOR * 1.0
-# Assumes we'll have 3 dendritic shafts, there was a problem with 4.
+# Assumes we'll have 4 dendritic shafts.
 DENDRITE_LEFT = - DENDRITE_RADIUS
-DENDRITE_RIGHT = 5 * DENDRITE_RADIUS
+DENDRITE_RIGHT = 7 * DENDRITE_RADIUS
 SHAFT_HORIZ_SHIFT = 2 * DENDRITE_RADIUS
 
 
 def get_shaft(shaft_index):
   curr_shift = (shaft_index - 1) * SHAFT_HORIZ_SHIFT
-  sphere = dolfin.Sphere(dolfin.Point(curr_shift, 0, SPHERE_Z),
-                         SPHERE_RADIUS)
-  cone = dolfin.Cone(dolfin.Point(curr_shift, 0, CYLINDER_BOTTOM),
-                     dolfin.Point(curr_shift, 0, CYLINDER_TOP),
-                     CYLINDER_RADIUS, CYLINDER_RADIUS)
-  box = dolfin.Box(
-      -BOX_WIDTH / 2 + curr_shift, -BOX_WIDTH / 2, BOX_BOTTOM,
-      BOX_WIDTH / 2 + curr_shift, BOX_WIDTH / 2, BOX_BOTTOM + BOX_WIDTH)
-  return (sphere - box) + cone
+  top_cone = dolfin.Cone(dolfin.Point(curr_shift, 0, CONE_BOUNDARY_Z),
+                         dolfin.Point(curr_shift, 0, TOP_CONE_TOP_Z),
+                         CONE_BOUNDARY_RADIUS, TOP_CONE_TOP_RADIUS)
+  bottom_cone = dolfin.Cone(dolfin.Point(curr_shift, 0, BOTTOM_CONE_BOTTOM_Z),
+                            dolfin.Point(curr_shift, 0, CONE_BOUNDARY_Z),
+                            BOTTOM_CONE_BOTTOM_RADIUS, CONE_BOUNDARY_RADIUS)
+  cylinder = dolfin.Cone(dolfin.Point(curr_shift, 0, CYLINDER_BOTTOM),
+                         dolfin.Point(curr_shift, 0, CYLINDER_TOP),
+                         CYLINDER_RADIUS, CYLINDER_RADIUS)
+  return top_cone + bottom_cone + cylinder
 
 
 dendrite_cone = dolfin.Cone(
@@ -47,10 +55,12 @@ dendrite_cone = dolfin.Cone(
 geometry_first_shaft = get_shaft(1)
 geometry_second_shaft = get_shaft(2)
 geometry_third_shaft = get_shaft(3)
+geometry_fourth_shaft = get_shaft(4)
 
 # Combine all geometries
 geometry_3d = (geometry_first_shaft + geometry_second_shaft +
-               geometry_third_shaft + dendrite_cone)
+               geometry_third_shaft + geometry_fourth_shaft +
+               dendrite_cone)
 dolfin.info(geometry_3d, True)
 resolution = 32
 print '=' * 50
