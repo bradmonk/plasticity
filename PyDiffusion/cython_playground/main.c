7a4218e8ac2b552accbe5f4bc13edae58b8aa71f
Danny Hermes <daniel.j.hermes@gmail.com>
2015-03-05 15:46:42 -0800

    Adding PyDiffusion/cython_playground/ to try to find segfault.

---
+++ PyDiffusion/cython_playground/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <Python.h>
+#include "foo.h"
+
+int main(void)
+{
+    Py_Initialize();
+    initfoo();
+    foo_stream(stderr);
+    // foo_direct();
+    Py_Finalize();
+    return 0;
+}
