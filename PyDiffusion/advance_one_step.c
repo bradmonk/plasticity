3f9aededba3c8d63b4cd766dd00ec846f89c1a4d
Danny Hermes <daniel.j.hermes@gmail.com>
2015-03-05 12:19:25 -0800

    Moving placeholder arguments to end of MEX signature.

--- PyDiffusion/advance_one_step.c
+++ PyDiffusion/advance_one_step.c
@@ -9,11 +9,10 @@
  *
  * The calling syntax is:
  *
- *     outMatrix = arrayProduct(multiplier, inMatrix, xyz_loc, ...
- *                              face_indices, k, initial_point, ...
+ *     outMatrix = arrayProduct(xyz_loc, face_indices, k, initial_point, ...
  *                              initial_face_index, all_vertices, ...
  *                              triangles, face_local_bases, ...
- *                              neighbor_faces)
+ *                              neighbor_faces, multiplier, inMatrix)
  *
  *========================================================
  */
@@ -51,15 +50,15 @@ void mexFunction( int nlhs, mxArray *plhs[],
     size_t num_vertices;            /* size of all_vertices */
     size_t num_triangles;           /* size of triangles */
 
-    double *xyz_loc;                /* 2. Mx3 input matrix */
-    long *face_indices;             /* 3. Mx1 input matrix */
-    double k;                       /* 4. input scalar */
-    double *initial_point;          /* 5. 1x3 input matrix */
-    long initial_face_index;        /* 6. input scalar */
-    double *all_vertices;           /* 7. Vx3 input matrix */
-    long *triangles;                /* 8. Tx3 input matrix */
-    double *face_local_bases;       /* 9. Tx6 input matrix */
-    long *neighbor_faces;           /* 10. Tx3 input matrix */
+    double *xyz_loc;                /* 0. Mx3 input matrix */
+    long *face_indices;             /* 1. Mx1 input matrix */
+    double k;                       /* 2. input scalar */
+    double *initial_point;          /* 3. 1x3 input matrix */
+    long initial_face_index;        /* 4. input scalar */
+    double *all_vertices;           /* 5. Vx3 input matrix */
+    long *triangles;                /* 6. Tx3 input matrix */
+    double *face_local_bases;       /* 7. Tx6 input matrix */
+    long *neighbor_faces;           /* 8. Tx3 input matrix */
 
     /* check for proper number of arguments */
     if(nrhs!=11) {
@@ -70,122 +69,123 @@ void mexFunction( int nlhs, mxArray *plhs[],
     }
 
     /* make sure the first input argument is scalar */
-    if( !mxIsDouble(prhs[0]) ||
-         mxIsComplex(prhs[0]) ||
-         mxGetNumberOfElements(prhs[0])!=1 ) {
+    if( !mxIsDouble(prhs[9]) ||
+         mxIsComplex(prhs[9]) ||
+         mxGetNumberOfElements(prhs[9])!=1 ) {
         mexErrMsgIdAndTxt("advance_one_step:notScalar","Input multiplier must be a scalar.");
     }
 
     /* make sure the second input argument is type double */
-    if( !mxIsDouble(prhs[1]) ||
-         mxIsComplex(prhs[1])) {
+    if( !mxIsDouble(prhs[10]) ||
+         mxIsComplex(prhs[10])) {
         mexErrMsgIdAndTxt("advance_one_step:notDouble","Input matrix must be type double.");
     }
 
     /* check that number of rows in second input argument is 1 */
-    if(mxGetM(prhs[1])!=1) {
+    if(mxGetM(prhs[10])!=1) {
         mexErrMsgIdAndTxt("advance_one_step:notRowVector","Input must be a row vector.");
     }
 
     /* make sure xyz_loc is double array (of correct shape) */
-    if(mxGetN(prhs[2])!=3) {
+    if(mxGetN(prhs[0])!=3) {
         mexErrMsgIdAndTxt("advance_one_step:notXYZCoords",
                           "xyz_loc must have 3 columns.");
     }
-    if( !mxIsDouble(prhs[2]) ||
-         mxIsComplex(prhs[2])) {
+    if( !mxIsDouble(prhs[0]) ||
+         mxIsComplex(prhs[0])) {
         mexErrMsgIdAndTxt("advance_one_step:notDouble",
                           "xyz_loc must be type double.");
     }
 
     /* make sure face_indices is long array (of correct shape) */
-    if(mxGetN(prhs[3])!=1) {
+    if(mxGetN(prhs[1])!=1) {
         mexErrMsgIdAndTxt("advance_one_step:notColVector",
                           "face_indices must have 1 column.");
     }
     /* NOTE: We don't check that rows(face_indices) == rows(xyz_loc). */
-    if( !mxIsInt64(prhs[3]) ) {
+    if( !mxIsInt64(prhs[1]) ) {
         mexErrMsgIdAndTxt("advance_one_step:notLong",
                           "face_indices must be type long.");
     }
 
     /* make sure k is scalar double */
-    if( !mxIsDouble(prhs[4]) ||
-         mxIsComplex(prhs[4]) ||
+    if( !mxIsDouble(prhs[2]) ||
+         mxIsComplex(prhs[2]) ||
          mxGetNumberOfElements(prhs[4])!=1 ) {
         mexErrMsgIdAndTxt("advance_one_step:notScalarDouble",
                           "k must be a scalar double.");
     }
 
     /* make sure initial_point is double 1x3 array */
-    if(mxGetN(prhs[5])!=3 || mxGetM(prhs[5])!=1) {
+    if(mxGetN(prhs[3])!=3 || mxGetM(prhs[3])!=1) {
         mexErrMsgIdAndTxt("advance_one_step:notXYZVec",
                           "initial_point must be 1x3.");
     }
-    if( !mxIsDouble(prhs[5]) ||
-         mxIsComplex(prhs[5])) {
+    if( !mxIsDouble(prhs[3]) ||
+         mxIsComplex(prhs[3])) {
         mexErrMsgIdAndTxt("advance_one_step:notDouble",
                           "initial_point must be type double.");
     }
 
     /* make sure initial_face_index is a scalar long */
-    if( !mxIsInt64(prhs[6]) ||
-         mxGetNumberOfElements(prhs[6])!=1 ) {
+    if( !mxIsInt64(prhs[4]) ||
+         mxGetNumberOfElements(prhs[4])!=1 ) {
         mexErrMsgIdAndTxt("advance_one_step:notScalar",
                           "initial_face_index must be a scalar long.");
     }
 
     /* make sure all_vertices is double array (of correct shape) */
-    if(mxGetN(prhs[7])!=3) {
+    if(mxGetN(prhs[5])!=3) {
         mexErrMsgIdAndTxt("advance_one_step:notXYZCoords",
                           "all_vertices must have 3 columns.");
     }
-    if( !mxIsDouble(prhs[7]) ||
-         mxIsComplex(prhs[7])) {
+    if( !mxIsDouble(prhs[5]) ||
+         mxIsComplex(prhs[5])) {
         mexErrMsgIdAndTxt("advance_one_step:notDouble",
                           "all_vertices must be type double.");
     }
 
     /* make sure triangles is long array (of correct shape) */
-    if(mxGetN(prhs[8])!=3) {
+    if(mxGetN(prhs[6])!=3) {
         mexErrMsgIdAndTxt("advance_one_step:notVertexIndices",
                           "triangles must have 3 columns.");
     }
-    if( !mxIsInt64(prhs[8]) ) {
+    if( !mxIsInt64(prhs[6]) ) {
         mexErrMsgIdAndTxt("advance_one_step:notLong",
                           "triangles must be type long.");
     }
 
     /* make sure face_local_bases is double array (of correct shape) */
-    if(mxGetN(prhs[9])!=6) {
+    if(mxGetN(prhs[7])!=6) {
         mexErrMsgIdAndTxt("advance_one_step:notFaceBases",
                           "face_local_bases must have 6 columns.");
     }
     /* NOTE: We don't check that rows(face_local_bases) == rows(triangles). */
-    if( !mxIsDouble(prhs[9]) ||
-         mxIsComplex(prhs[9])) {
+    if( !mxIsDouble(prhs[7]) ||
+         mxIsComplex(prhs[7])) {
         mexErrMsgIdAndTxt("advance_one_step:notDouble",
                           "face_local_bases must be type double.");
     }
+
     /* make sure neighbor_faces is long array (of correct shape) */
-    if(mxGetN(prhs[10])!=3) {
+    if(mxGetN(prhs[8])!=3) {
         mexErrMsgIdAndTxt("advance_one_step:notNeighborIndices",
                           "neighbor_faces must have 3 columns.");
     }
     /* NOTE: We don't check that rows(neighbor_faces) == rows(triangles). */
-    if( !mxIsInt64(prhs[10]) ) {
+    if( !mxIsInt64(prhs[8]) ) {
         mexErrMsgIdAndTxt("advance_one_step:notLong",
                           "neighbor_faces must be type long.");
     }
 
     /* get the value of the scalar input  */
-    multiplier = mxGetScalar(prhs[0]);
+    multiplier = mxGetScalar(prhs[9]);
 
     /* create a pointer to the real data in the input matrix  */
-    inMatrix = mxGetPr(prhs[1]);
+    inMatrix = mxGetPr(prhs[10]);
 
     /* get dimensions of the input matrix */
-    ncols = mxGetN(prhs[1]);
+    ncols = mxGetN(prhs[10]);
 
     /* create the output matrix */
     plhs[0] = mxCreateDoubleMatrix(1,(mwSize)ncols,mxREAL);
