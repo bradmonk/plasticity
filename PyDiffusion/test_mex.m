e585f8ffd9893729a6a666f7a92c7386f703d34a
Danny Hermes <daniel.j.hermes@gmail.com>
2015-03-08 11:18:47 -0700

    Adding custom display to test_mex.m

--- PyDiffusion/test_mex.m
+++ PyDiffusion/test_mex.m
@@ -3,4 +3,8 @@ xyz_loc = initial_point;
 face_indices = [initial_face_index];
 [xyz_loc, face_indices] = advance_one_step(...
     xyz_loc, face_indices, k, initial_point, initial_face_index, ...
-    all_vertices, triangles, face_local_bases, neighbor_faces)
+    all_vertices, triangles, face_local_bases, neighbor_faces);
+disp('xyz_loc:');
+disp(xyz_loc);
+disp('face_indices:');
+disp(face_indices);
