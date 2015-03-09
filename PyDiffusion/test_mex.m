a3e0c714439263a7c88079a6cd28ec8a779608e6
Danny Hermes <daniel.j.hermes@gmail.com>
2015-03-08 19:53:34 -0700

    Adding some small notes to test_mex.m.

--- PyDiffusion/test_mex.m
+++ PyDiffusion/test_mex.m
@@ -1,6 +1,11 @@
 load('data/serialized_mesh_res_96.mat');
+k = 6.0;  % Override k.
+
+% Run simulation on just a single point.
+% We need xyz_loc and face_indices for this point.
 xyz_loc = initial_point;
 face_indices = [initial_face_index];
+
 [xyz_loc, face_indices] = advance_one_step(...
     xyz_loc, face_indices, k, initial_point, initial_face_index, ...
     all_vertices, triangles, face_local_bases, neighbor_faces);
