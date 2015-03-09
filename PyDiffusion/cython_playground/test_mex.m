23889174ad37560ebe8de2745f8a15d617f0b457
Danny Hermes <daniel.j.hermes@gmail.com>
2015-03-08 17:49:07 -0700

    Removing cython_playground.
    Moved to dedicated repository (to serve as a future example):
    https://github.com/dhermes/python-from-matlab

--- PyDiffusion/cython_playground/test_mex.m
+++
@@ -1,9 +0,0 @@
-zap = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
-disp('zap before:');
-disp(zap);
-zap_rows = 3;
-zap_new = foo_mex(zap, zap_rows);
-disp('foo_mex(zap, 3):');
-disp(zap_new);
-disp('zap after:');
-disp(zap);
